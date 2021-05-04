e74c7a043f953bf126831dfdc97963c187f2bf33
Sunny <sl2883@cornell.edu>
2021-05-04 12:27:10 -0700

    adding CT with login event

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 'use strict'
-
+ 
 const switcher = document.querySelector('.btn');
 
 switcher.addEventListener('click', function() {
@@ -8,9 +8,32 @@ switcher.addEventListener('click', function() {
     var className = document.body.className;
     if(className == "light-theme") {
         this.textContent = "Dark";
+
+        clevertap.onUserLogin.push({
+            "Site": {
+              "Name": "SL Web May 1",            // String
+              "Email": "slwebmay1@gmail.com",         // Email address of the user
+              "MSG-email": true,                // Disable email notifications
+              "MSG-push": true,                  // Enable push notifications
+              "MSG-sms": true,                   // Enable sms notifications
+              "MSG-whatsapp": true,              // Enable WhatsApp notifications
+            }
+           });
     }
     else {
         this.textContent = "Light";
+
+        clevertap.onUserLogin.push({
+            "Site": {
+              "Name": "SL Web May 2",            // String
+              "Email": "slwebmay2@gmail.com",         // Email address of the user
+              "MSG-email": true,                // Disable email notifications
+              "MSG-push": true,                  // Enable push notifications
+              "MSG-sms": true,                   // Enable sms notifications
+              "MSG-whatsapp": true,              // Enable WhatsApp notifications
+            }
+           });
+        
     }
 
 });
